5ced85a39b90a40e6ed77b1d94eccfa22423dd90
Cronabit 1 <cronabit1@Cronabits-Mac-mini.local>
2017-10-28 15:07:40 +0530

    Initial Commit

---
+++ mygame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mygame
+//
+//  Created by Cronabit 1 on 28/10/17.
+//  Copyright © 2017 Cronabit 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
